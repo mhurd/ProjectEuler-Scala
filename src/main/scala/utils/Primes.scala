35612649441e64365b0904ddebbfe934b473ecbe
mhurd <mjwhurd@gmail.com>
2014-04-22 13:50:00 +0100

    Added fermat's little theorem implementation

--- src/main/scala/utils/Primes.scala
+++ src/main/scala/utils/Primes.scala
@@ -1,11 +1,14 @@
 package utils
 
 import scala.annotation.tailrec
+import scala.util.Random
+import scala.math
 
 
 object Primes {
 
   lazy val seedPrimes = sieveOfEratosthenes(naturalNumberStream(2)).take(100).toList
+  lazy val rand = new Random()
 
   def isPrime(n: Long): Boolean = {
     if (n < 2) false
@@ -20,8 +23,21 @@ object Primes {
     true
   }
 
+  def fermatsLittleTheoremIsPrime(n: Long): Boolean = {
+    // shortcut the smaller primes
+    if (n == 1) false else if (List(2,3,5,7,11,13,17,19,23,29).contains(n)) true else {
+      (1 to 10).map(f = _ => {
+        val l: Long = Math.abs(rand.nextLong())
+        val r: Long = if (l == 0) 1 else (n * (l.toDouble / Long.MaxValue)).toLong
+        // wow, handy modPow function on BigInt!
+        val isWitness = BigInt(r).modPow(BigInt(n-1), BigInt(n)) != BigInt(1)
+        isWitness
+      }).forall(_ == false)
+    }
+  }
+
   // Use some tricks here, pre-compute the first 100 primes an use these
-  // to quickly check if a candidate number is composite other wise proceed to
+  // to quickly check if a candidate number is composite otherwise proceed to
   // the lengthy check only if we're still not sure.
   def nthPrime(n: Int): Long = {
     if (n <= seedPrimes.size) {
--- src/test/scala/utils/PrimesSpec.scala
+++ src/test/scala/utils/PrimesSpec.scala
@@ -20,4 +20,39 @@ class PrimesSpec extends FlatSpec with Matchers {
     Primes.nthPrime(10) should be(29)
   }
 
+  "The 'fermatsLittleTheorem' method" should "return detect primes correctly" in {
+    (Primes.fermatsLittleTheoremIsPrime(1)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(2)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(3)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(4)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(5)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(6)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(7)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(8)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(9)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(10)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(11)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(12)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(13)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(14)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(15)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(16)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(17)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(18)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(19)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(20)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(21)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(22)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(23)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(29)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(30)) should be(false)
+    // larger primes
+    (Primes.fermatsLittleTheoremIsPrime(1301081)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(179426081)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(2038076783)) should be(true)
+    (Primes.fermatsLittleTheoremIsPrime(1301089)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(179426090)) should be(false)
+    (Primes.fermatsLittleTheoremIsPrime(2038076795)) should be(false)
+  }
+
 }
